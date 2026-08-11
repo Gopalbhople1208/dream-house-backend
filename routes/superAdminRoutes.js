3402db719e4dc5ce4ef8abccd5efb436a13e6496
Gopalbhople <gbhople63@gmail.com>
2026-08-11 18:05:03 +0530

    this check is

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -18,8 +18,32 @@
 // );
 // module.exports = router;
 
-const express = require("express");
+// const express = require("express");
 
+// const router = express.Router();
+
+// const {
+//   loginSuperAdmin,
+//   createSuperAdmin,
+//   getProfile,
+//   updateProfileImage,
+// } = require("../controllers/superAdminController");
+
+// const upload = require("../middleware/upload");
+
+// router.post("/create", createSuperAdmin);
+
+// router.post("/login", loginSuperAdmin);
+
+// router.get("/:id", getProfile);
+
+// router.put(
+//   "/:id/profile-image",
+//   upload.single("profileImage"),
+//   updateProfileImage
+// );
+// module.exports = router;
+const express = require("express");
 const router = express.Router();
 
 const {
